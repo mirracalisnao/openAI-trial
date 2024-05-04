11e433e128b2f1f1176dde6350a724d5c1b4c2ec
cherrymirracalisnao <cherrymirra.calisnao@wvsu.edu.ph>
2024-05-04 12:57:18 +0800

    e

--- med_helper.py
+++ med_helper.py
@@ -80,7 +80,7 @@ async def display_symptoms_form1():
             else:
                 st.session_state["selected_medication"] = selected_medication  # Save selected medication
                 st.session_state["current_form"] = 2  # Move to the next form
-            await display_information3()  # Call the display_information3 function directly
+            await display_information2()  # Call the display_information3 function directly
         else:
             form1.warning("Please enter your symptoms.")       
 
